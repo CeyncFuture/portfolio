d1179ff249dab92f124f214c17b769c24f645424
Chanaka-Madushan-Herath <chanaka.herath.1998@gmail.com>
2024-03-09 12:40:01 +0530

    added footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,26 @@
+import logo from '../assets/images/CeyncFuture-white.png'
+import {contactDetails} from "../utils/Constants";
+import KeyboardDoubleArrowUpIcon from '@mui/icons-material/KeyboardDoubleArrowUp';
+
 const Footer = () => {
     return(
-    <div>
-        footer
-    </div>
+        <>
+            <button className="to-top-button" onClick={()=>window.scrollTo(0,0)}>
+                <KeyboardDoubleArrowUpIcon/>
+                <span>Top</span>
+            </button>
+            <div className="footer-container">
+            <img className="footer-logo" src={logo}/>
+            <div className="Contact-details">
+                <div>{contactDetails.email}</div>
+                <div>{contactDetails.phoneNumbers}</div>
+            </div>
+            <div className="copyright-message">
+                &copy; {`${new Date().getFullYear()} ${contactDetails.name}`}. All rights reserved.
+            </div>
+        </div>
+        </>
+
     )
 }
 
---
+++ src/utils/Constants.js
@@ -0,0 +1,5 @@
+export  const contactDetails = {
+    name: 'CeyncFuture',
+    email: 'ceyncfuture@gmail.com',
+    phoneNumbers: '+94 77 974 0107 / +94 76 702 5709'
+}
