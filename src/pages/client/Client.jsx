9e28ad4c3cacf9a69399a3562952b00439fcdb4a
pahalagedara <thilinam4@gmail.com>
2024-03-19 20:00:50 +0530

    add parallax

--- src/pages/client/Client.jsx
+++ src/pages/client/Client.jsx
@@ -1,19 +1,31 @@
 import React, {lazy} from 'react';
+import { ParallaxProvider, ParallaxBanner } from 'react-scroll-parallax';
+
+import bg_1 from '../../assets/images/background/bg_1.jpg';
+
 import Footer from '../../components/Footer';
 import Navbar from '../../components/Navbar';
-// import CardSlider from '../../components/CardSlider';
+
 const CardSlider = lazy(()=>import('../../components/CardSlider'));
 
 const RootPage = () => {
   return (
-    <div className='client'>
-        <Navbar/>
-        <div className='container'>
-          <CardSlider />
-          {/* <motion.div style={{height:'500px',backgroundColor:'white'}} id='white'></motion.div> */}
-        </div>
-        <Footer />
-    </div>
+    <ParallaxProvider>
+      <div className='client'>
+          <Navbar/>
+          <div className='container'>
+            <CardSlider />
+            <ParallaxBanner 
+              layers={[
+                {image: bg_1, speed: -20}
+              ]}>
+                <CardSlider />
+                <div style={{height:'700px',backgroundColor:'red', width: '100%'}} ></div>
+            </ParallaxBanner>
+          </div>
+          <Footer />
+      </div>
+    </ParallaxProvider>
   )
 }
 
