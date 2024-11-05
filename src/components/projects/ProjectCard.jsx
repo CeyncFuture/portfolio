68f030f6b29a015b8ae9f82833e8d3a186df1b03
pahalagedara <thilinam4@gmail.com>
2024-11-05 23:56:17 +0530

    sojksljdk

--- src/components/projects/ProjectCard.jsx
+++ src/components/projects/ProjectCard.jsx
@@ -1,33 +1,35 @@
-import Button from "../reusable/Button";
-import {Link} from "react-router-dom";
+// import Button from "../reusable/Button";
+// import {Link} from "react-router-dom";
 
-const ProjectCard = ({id, categories, title, desc, imgUrl}) => {
+const ProjectCard = ({id, categories, title, desc, imgUrl, isLeftAligned}) => {
+  const alignment = isLeftAligned ? 'flex-row' : 'flex-row-reverse';
     return (
-        <div className='flex mb-4 items-center'>
-            <div className="mx-2" style={{minWidth: '450px'}}>
+        <div className={`flex flexWrap justify-center mb-4 items-center ${alignment}`}>
+            <div className="mx-2" style={{maxWidth: '300px'}}>
                 <img src={imgUrl}  className="rounded-xl border-none" alt={title}/>
             </div>
-            <div className="p-6">
-                <div className="mb-3">
-                    {categories.map((category, index) =>
-                        <h1
-                            key={index}
-                            className="bg-indigo-500 font-bold text-white p-3 rounded-md"
-                            style={{ width: "fit-content"}}
-                        >
-                            {category}
-                        </h1>
-                    )}
-                </div>
+            <div className="p-6" style={{maxWidth: '750px'}}>
                 <h1 className="font-general-medium text-lg font-bold md:text-xl text-ternary-dark dark:text-ternary-light mb-2">{title}</h1>
+                <div className="mb-3">
+                  {categories.map((category, index) =>
+                    <h1
+                      key={index}
+                      className="bg-indigo-500 font-medium text-white p-2 rounded-md"
+                      style={{ width: "fit-content", fontSize: "12px"}}
+                    >
+                      {category}
+                    </h1>
+                  )}
+                </div>
                 <p className="text-lg text-ternary-dark dark:text-ternary-light mb-3">{desc}</p>
-                <Link
-                    to={`/project/${id}`}
-                    className="font-general-medium rounded-lg focus:ring-1 focus:ring-indigo-900 text-ternary-dark text-lg sm:text-md duration-300"
+                {/* <Link
+                    //to={`/project/single-project`}
+                    className="font-general-medium rounded-lg focus:ring-1 focus:ring-indigo-900 text-ternary-dark dark:text-ternary-light text-lg sm:text-md duration-300"
                     aria-label="Learn More"
                 >
                     <Button title="Learn More..."/>
-                </Link>
+                </Link> */}
+              <hr className="my-12 h-0.5 border-t-0 bg-neutral-100 dark:bg-white/10" style={{marginTop: "15px"}}/>
             </div>
         </div>
     );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import { Link } from 'react-router-dom';
 import AppBanner from '../components/shared/AppBanner';
-import ProjectsGrid from '../components/projects/ProjectsGrid';
-import { ProjectsProvider } from '../context/ProjectsContext';
+// import ProjectsGrid from '../components/projects/ProjectsGrid';
+// import { ProjectsProvider } from '../context/ProjectsContext';
 import Button from '../components/reusable/Button';
 import TechSlider from '../components/TechSlider';
 import { techIcons } from '../data/sliders';
 import {projectsData2} from "../data/projects";
 import ProjectCard from "../components/projects/ProjectCard";
+import About from "./AboutMe";
+import Contact from "./Contact";
 
 const Home = () => {
 
@@ -15,11 +17,16 @@ const Home = () => {
 			<AppBanner />
 			<TechSlider techIcons={techIcons}/>
 
-			<ProjectsProvider>
+			{/*<ProjectsProvider>
 				<ProjectsGrid />
-			</ProjectsProvider>
+			</ProjectsProvider>*/}
 
-			<div className="projects">
+			<div id="projects" className="projects">
+				<div className="text-center" style={{marginTop: "13vh"}}>
+					<p className="font-general-medium text-2xl sm:text-4xl mb-1 text-ternary-dark dark:text-ternary-light" style={{marginBottom: "7vh"}}>
+						Projects portfolio
+					</p>
+				</div>
 				{
 					projectsData2.map((project, index) =>
 						<ProjectCard
@@ -28,19 +35,36 @@ const Home = () => {
 							title={project.title}
 							desc={project.desc}
 							imgUrl={project.img}
+							isLeftAligned={index % 2 === 0}
 						/>
 					)
 				}
 			</div>
 
 			<div className="mt-8 sm:mt-10 flex justify-center">
-				<Link
-					to="/projects"
+				<a
+					href="/projects"
 					className="font-general-medium flex items-center px-6 py-3 rounded-lg shadow-lg hover:shadow-xl bg-indigo-500 hover:bg-indigo-600 focus:ring-1 focus:ring-indigo-900 text-white text-lg sm:text-xl duration-300"
 					aria-label="More Projects"
 				>
 					<Button title="More Projects" />
-				</Link>
+				</a>
+			</div>
+			<div id="about" className="about">
+				<div className="text-center" style={{marginTop: "13vh"}}>
+					<p className="font-general-medium text-2xl sm:text-4xl mb-1 text-ternary-dark dark:text-ternary-light" style={{marginBottom: "7vh"}}>
+						Who are we...
+					</p>
+				</div>
+			<About />
+			</div>
+			<div id="contact" className="contact">
+				<div className="text-center" style={{marginTop: "13vh"}}>
+					<p className="font-general-medium text-2xl sm:text-4xl mb-1 text-ternary-dark dark:text-ternary-light" style={{marginBottom: "7vh"}}>
+						Let's talk...
+					</p>
+				</div>
+			<Contact />
 			</div>
 		</div>
 	);
