b410c3c9e46b4cbdabd69babe0773401d93af23d
pahalagedara <thilinam4@gmail.com>
2024-11-06 01:29:45 +0530

    mata pissu

--- src/components/about/AboutClients.jsx
+++ src/components/about/AboutClients.jsx
@@ -40,8 +40,8 @@ const AboutClients = () => {
 	  return (
 		<section className="text-center py-12 px-6 mt-10">
 		  {/* Title and Description */}
-		  <h2 className="text-2xl font-semibold mb-2">Meet our leadership</h2>
-		  <p className="text-gray-600 max-w-xl mx-auto mb-12">
+		  <h2 className=" text-primary-dark dark:text-primary-light text-2xl font-semibold mb-2">Meet our leadership</h2>
+		  <p className="text-gray-600 dark:text-primary-light max-w-xl mx-auto mb-12">
 			We’re a dynamic group of individuals who are passionate about what we do and dedicated to delivering the best results for our clients.
 		  </p>
 	
@@ -54,13 +54,13 @@ const AboutClients = () => {
 				  alt={member.name}
 				  className="w-32 h-32 rounded-full object-cover mb-4"
 				/>
-				<h3 className="text-lg font-semibold">{member.name}</h3>
-				<p className="text-gray-500">{member.role}</p>
+				<h3 className="text-lg text-primary-dark dark:text-primary-light font-semibold">{member.name}</h3>
+				<p className="text-gray-500 dark:text-primary-light  ">{member.role}</p>
 				<div className="flex gap-4 mt-4 mb-5">
-				  <a href={member.twitter} target="_blank" rel="noopener noreferrer" className="text-gray-500 hover:text-blue-500">
+				  <a href={member.twitter} target="_blank" rel="noopener noreferrer" className="text-gray-500 dark:text-primary-light   hover:text-blue-500">
 					<FaTwitter size={20} />
 				  </a>
-				  <a href={member.linkedin} target="_blank" rel="noopener noreferrer" className="text-gray-500 hover:text-blue-700">
+				  <a href={member.linkedin} target="_blank" rel="noopener noreferrer" className="text-gray-500 dark:text-primary-light  hover:text-blue-700">
 					<FaLinkedin size={20} />
 				  </a>
 				</div>
