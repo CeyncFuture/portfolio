5db0721cc3c333d60358d84c7b2c8b388d099029
CharithNiroshan <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>
2024-11-07 23:50:01 +0530

    Updated footer

--- src/components/shared/AppFooter.jsx
+++ src/components/shared/AppFooter.jsx
@@ -1,82 +1,87 @@
-import {
-	FiTwitter,
-	FiLinkedin,
-	FiGlobe,
-	FiYoutube,
-	FiFacebook,
-} from 'react-icons/fi';
-import AppFooterCopyright from './AppFooterCopyright';
+import logoLight from '../../images/logo-light.png';
+import logoDark from '../../images/logo-dark.png';
 import Modal from "../reusable/Modal";
 import PrivacyPolicy from "../../pages/PrivacyPolicy";
 import TermsOfUse from "../../pages/TermsOfUse";
-
-const socialLinks = [
-	{
-		id: 1,
-		icon: <FiGlobe />,
-		url: 'https://ceyncfuture.live/',
-	},
-	{
-		id: 2,
-		icon: <FiFacebook />,
-		url: 'https://github.com/realstoman',
-	},
-	{
-		id: 3,
-		icon: <FiTwitter />,
-		url: 'https://twitter.com/',
-	},
-	{
-		id: 4,
-		icon: <FiLinkedin />,
-		url: 'https://www.linkedin.com/',
-	},
-	{
-		id: 5,
-		icon: <FiYoutube />,
-		url: 'https://www.youtube.com/',
-	},
-];
+import useThemeSwitcher from "../../hooks/useThemeSwitcher";
 
 const AppFooter = () => {
+	const [activeTheme] = useThemeSwitcher();
 	return (
-		<div className="container mx-auto">
-			<div className="pt-20 sm:pt-30 pb-8 mt-20 border-t-2 border-primary-light dark:border-secondary-dark">
-				{/* Footer social links */}
-				<div className="font-general-regular flex flex-col justify-center items-center mb-12 sm:mb-28">
-					<p className="text-3xl sm:text-4xl text-primary-dark dark:text-primary-light mb-5">
-						Follow Us
-					</p>
-					<ul className="flex gap-4 sm:gap-8">
-						{socialLinks.map((link) => (
-							<a
-								href={link.url}
-								target="__blank"
-								key={link.id}
-								className="text-gray-400 hover:text-indigo-500 dark:hover:text-indigo-400 cursor-pointer rounded-lg bg-gray-50 dark:bg-ternary-dark hover:bg-gray-100 shadow-sm p-4 duration-300"
-							>
-								<i className="text-xl sm:text-2xl md:text-3xl">
-									{link.icon}
-								</i>
-							</a>
-						))}
-					</ul>
+		<footer className="footer-bg dark:bg-gray-900">
+			<div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
+				<div className="md:flex md:justify-between">
+					<div className="mb-6 md:mb-0">
+						<a href="#" className="flex items-center">
+							{activeTheme === 'dark' ? (
+								<img
+									src={logoDark}
+									className="h-36 me-3" alt="Logo"
+									alt="Dark Logo"
+								/>
+							) : (
+								<img
+									src={logoLight}
+									className="h-36 me-3" alt="Logo"
+									alt="Dark Logo"
+								/>
+							)}
+						</a>
+					</div>
+					<div className="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
+						<div>
+							<h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Resources</h2>
+							<ul className="text-gray-500 dark:text-gray-400 font-medium">
+								<li className="mb-4">
+									<a href="https://flowbite.com/" className="hover:underline">Flowbite</a>
+								</li>
+								<li>
+									<a href="https://tailwindcss.com/" className="hover:underline">Tailwind CSS</a>
+								</li>
+							</ul>
+						</div>
+						<div>
+							<h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Follow us</h2>
+							<ul className="text-gray-500 dark:text-gray-400 font-medium">
+								<li className="mb-4">
+									<a href="https://github.com/themesberg/flowbite" className="hover:underline ">Github</a>
+								</li>
+								<li>
+									<a href="https://discord.gg/4eeurUVvTy" className="hover:underline">Discord</a>
+								</li>
+							</ul>
+						</div>
+						<div>
+							<h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Legal</h2>
+							<ul className="text-gray-500 dark:text-gray-400 font-medium">
+								<li className="mb-4">
+									<Modal
+										button="Privacy Policy"
+										header="Privacy Policy"
+										body={<PrivacyPolicy/>}
+										id="privacy"
+									/>
+								</li>
+								<li>
+									<Modal
+										button="Terms of Use"
+										header="Terms of Use"
+										body={<TermsOfUse/>}
+										id="terms"
+									/>
+								</li>
+							</ul>
+						</div>
+					</div>
 				</div>
-				<Modal
-					button="Privacy Policy"
-					header="Privacy Policy"
-					body={<PrivacyPolicy/>}
-					id="privacy"
-				/>
-				<Modal
-					button="Terms of Use"
-					header="Terms of Use"
-					body={<TermsOfUse/>}
-					id="terms"
-				/>
-				<AppFooterCopyright />
+				<hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8"/>
+				<div className="sm:flex sm:items-center sm:justify-between">
+          <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2024 <a
+			  href="https://flowbite.com/" className="hover:underline">CeyncFuture™</a>. All Rights Reserved.
+          </span>
+				</div>
 			</div>
-		</div>
+		</footer>
 	);
 };
 
