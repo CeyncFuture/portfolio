d072f66b4a0c252a22252d5aaae29e9845a3ab35
pahalagedara <thilinam4@gmail.com>
2024-11-07 21:31:51 +0530

    installed flowbit

--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -4,6 +4,7 @@ import AboutClients from '../components/about/AboutClients';
 import { AboutMeProvider } from '../context/AboutMeContext';
 import { motion } from 'framer-motion';
 import {people} from "../data/projects";
+import Timeline from '../components/about/Timeline';
 
 const About = () => {
 	return (
@@ -33,6 +34,15 @@ const About = () => {
 				className="container mx-auto"
 			>
 				<AboutClients />
+			</motion.div>
+
+			<motion.div
+				initial={{ opacity: 0 }}
+				animate={{ opacity: 1, delay: 1 }}
+				exit={{ opacity: 0 }}
+				className="container mx-auto min-h-screen flex items-center justify-center bg-gray-900"
+			>
+				<Timeline />
 			</motion.div>
 		</AboutMeProvider>
 	);
