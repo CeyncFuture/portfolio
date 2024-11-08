b71c89dee61b2422aec8929800f11b8cda897d63
pahalagedara <thilinam4@gmail.com>
2024-11-08 21:44:38 +0530

    changed: timeline item position

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,7 +13,7 @@ module.exports = {
 				'secondary-light': '#FFFFFF',
 				'ternary-light': '#f6f7f8',
 
-				'primary-dark': '#0D2438',
+				'primary-dark': '#0D2435',
 				'secondary-dark': '#102D44',
 				'ternary-dark': '#1E3851',
 				'quaternary-dark': '#1E3A8A'
