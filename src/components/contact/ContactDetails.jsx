6634b15457e94d0bb7422e4da6ad067ad367a490
pahalagedara <thilinam4@gmail.com>
2024-11-20 23:07:53 +0530

    FIxed: contact details issue

--- src/components/contact/ContactDetails.jsx
+++ src/components/contact/ContactDetails.jsx
@@ -21,7 +21,7 @@ const contacts = [
 const ContactDetails = () => {
   return (
     <div className="w-full lg:w-1/2">
-      <div className="text-left max-w-xl px-6">
+      <div className="w-fit m-auto px-6 text-center">
         <div className="text-center lg:hidden">
           <p className="font-general-medium text-2xl text-ternary-dark dark:text-ternary-light mb-5">
             Get In Touch
@@ -31,31 +31,34 @@ const ContactDetails = () => {
             to you.
           </p>
         </div>
-        <h2 className="font-general-medium text-xl text-primary-dark dark:text-primary-light mt-12 mb-8">
+        {/* <div className="flex"> */}
+        <div className="hidden lg:block text-center mt-24 mb-24">
+        <p className="font-general-medium text-2xl sm:text-4xl mb-1 text-ternary-dark dark:text-ternary-light">
+            Get In Touch
+          </p>
+          <p className="font-general-regular text-lg dark:text-primary-light mt-10">
+            Need help? <br /> just leave us a message, <br /> and we'll get back
+            to you.
+          </p>
+        </div>
+        {/* <h2 className="font-general-medium text-xl text-primary-dark dark:text-primary-light mt-12 mb-8">
           Contact details
-        </h2>
-        <ul className="font-general-regular">
+        </h2> */}
+
+        <ul className="font-general-regular mt-10">
           {contacts.map((contact) => (
             <li className="flex " key={contact.id}>
               <i className="text-2xl text-gray-500 dark:text-gray-400 mr-4">
                 {contact.icon}
               </i>
-              <span className="text-lg mb-4 text-ternary-dark dark:text-ternary-light">
+              <span className="text-lg mb-4 text-ternary-dark dark:text-ternary-light min-w-fit">
                 {contact.name}
               </span>
             </li>
           ))}
         </ul>
-        <div className="hidden lg:block text-center mt-20">
-          <p className="font-general-medium text-2xl mb-1 text-ternary-dark dark:text-ternary-light">
-            Get In Touch
-          </p>
-          <p className="font-general-medium text-lg dark:text-primary-light">
-            Need help? <br /> just leave us a message, <br /> and we'll get back
-            to you.
-          </p>
         </div>
-      </div>
+      {/* </div> */}
     </div>
   );
 };
